fa95c02d24ced5d8b416d2ee01100baabede6225
mtitajev <mtitajev@gmail.com>
2022-01-26 10:58:54 +0200

    pievienoju savu grafiku

--- GitNetUzdevums/Form1.Designer.cs
+++ GitNetUzdevums/Form1.Designer.cs
@@ -32,8 +32,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBoxMTitajevs = new System.Windows.Forms.GroupBox();
+            this.buttonMTitajevs = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.GroupBoxMTitajevs.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -75,17 +78,42 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Andrejs Liepiņš";
             // 
+            // GroupBoxMTitajevs
+            // 
+            this.GroupBoxMTitajevs.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.GroupBoxMTitajevs.Controls.Add(this.buttonMTitajevs);
+            this.GroupBoxMTitajevs.Location = new System.Drawing.Point(12, 97);
+            this.GroupBoxMTitajevs.Name = "GroupBoxMTitajevs";
+            this.GroupBoxMTitajevs.Size = new System.Drawing.Size(515, 292);
+            this.GroupBoxMTitajevs.TabIndex = 4;
+            this.GroupBoxMTitajevs.TabStop = false;
+            this.GroupBoxMTitajevs.Text = "Markuss Titajevs";
+            // 
+            // buttonMTitajevs
+            // 
+            this.buttonMTitajevs.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.buttonMTitajevs.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.buttonMTitajevs.Location = new System.Drawing.Point(6, 19);
+            this.buttonMTitajevs.Name = "buttonMTitajevs";
+            this.buttonMTitajevs.Size = new System.Drawing.Size(122, 51);
+            this.buttonMTitajevs.TabIndex = 0;
+            this.buttonMTitajevs.Text = "y=Cosx+2";
+            this.buttonMTitajevs.UseVisualStyleBackColor = false;
+            this.buttonMTitajevs.Click += new System.EventHandler(this.buttonMTitajevs_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 415);
+            this.Controls.Add(this.GroupBoxMTitajevs);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Funkciju grafiki";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.GroupBoxMTitajevs.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +124,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox GroupBoxMTitajevs;
+        private System.Windows.Forms.Button buttonMTitajevs;
     }
 }
 
