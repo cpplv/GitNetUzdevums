6cea4b7d25f23d37ac5fabf6a93ba7425de9be40
beatrisebertule <97166445+beatrisebertule@users.noreply.github.com>
2022-02-02 13:13:51 +0200

    Jauna funkcija y=2*x+1

--- GitNetUzdevums/Form1.Designer.cs
+++ GitNetUzdevums/Form1.Designer.cs
@@ -32,8 +32,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxBBertule = new System.Windows.Forms.GroupBox();
+            this.buttonBBertule = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBoxBBertule.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -75,17 +78,39 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Andrejs Liepiņš";
             // 
+            // groupBoxBBertule
+            // 
+            this.groupBoxBBertule.Controls.Add(this.buttonBBertule);
+            this.groupBoxBBertule.Location = new System.Drawing.Point(13, 81);
+            this.groupBoxBBertule.Name = "groupBoxBBertule";
+            this.groupBoxBBertule.Size = new System.Drawing.Size(200, 100);
+            this.groupBoxBBertule.TabIndex = 4;
+            this.groupBoxBBertule.TabStop = false;
+            this.groupBoxBBertule.Text = "GroupBoxBBertule";
+            // 
+            // buttonBBertule
+            // 
+            this.buttonBBertule.Location = new System.Drawing.Point(7, 20);
+            this.buttonBBertule.Name = "buttonBBertule";
+            this.buttonBBertule.Size = new System.Drawing.Size(75, 23);
+            this.buttonBBertule.TabIndex = 0;
+            this.buttonBBertule.Text = "y=2*x+1";
+            this.buttonBBertule.UseVisualStyleBackColor = true;
+            this.buttonBBertule.Click += new System.EventHandler(this.buttonBBertule_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 415);
+            this.Controls.Add(this.groupBoxBBertule);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Funkciju grafiki";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.groupBoxBBertule.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +121,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxBBertule;
+        private System.Windows.Forms.Button buttonBBertule;
     }
 }
 
