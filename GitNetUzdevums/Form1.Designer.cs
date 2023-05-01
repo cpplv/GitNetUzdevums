9f895afb43486b15acb05b3333950a94c6350410
Anita Liva <anita.liva39@gmail.com>
2023-05-01 09:52:08 +0300

    Pievienots jaunās pogas funkcijas formulas y=x*x*x kods

--- GitNetUzdevums/Form1.Designer.cs
+++ GitNetUzdevums/Form1.Designer.cs
@@ -96,6 +96,7 @@
             this.button3.TabIndex = 0;
             this.button3.Text = "y=x*x*x";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
